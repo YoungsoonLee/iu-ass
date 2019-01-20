936b9d250d56387ddbd2402ccf85678b6e4c755f
JayJay <youngtip@gmail.com>
2019-01-20 14:54:46 +0900

    .

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,24 +1,21 @@
-"use strict";
+'use strict';
 
-var _koaRouter = require("koa-router");
+var _koaRouter = require('koa-router');
 
 var _koaRouter2 = _interopRequireDefault(_koaRouter);
 
-var _v = require("./v1.0");
+var _v = require('./v1.0');
 
 var _v2 = _interopRequireDefault(_v);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-require("babel-core/register"); /* eslint-disable import/first */
+/* eslint-disable import/first */
 /* eslint-disable import/no-extraneous-dependencies */
-
-require("babel-polyfill");
+var api = new _koaRouter2.default();
 
 // for versioning
 
-
-var api = new _koaRouter2.default();
 api.use(_v2.default.routes());
 
 module.exports = api;
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,6 @@
 /* eslint-disable import/first */
 /* eslint-disable import/no-extraneous-dependencies */
-
 import Router from 'koa-router';
-
-require("babel-core/register");
-require("babel-polyfill");
 
 // for versioning
 import v1 from './v1.0';
